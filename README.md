84758274ab10d28ae166dae6fc3eef7b642e958e
sanazniamati <sanaz.niamati1991@gmail.com>
2022-07-30 16:53:00 +0430

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# React Konva
+
+React Konva is a JavaScript library for drawing complex canvas graphics using [React](https://reactjs.org/)
+
+# picture Demo
+
+![Demo](./img.png)
+
+**increase and decrease width and height of rectangel when click**
---
+++ src/index.js
@@ -0,0 +1,55 @@
+import React, { useRef, useState } from "react";
+import ReactDOM from "react-dom/client";
+import { Stage, Layer, Text, Rect } from "react-konva";
+
+const App = () => {
+  const [text, setText] = useState("");
+  const [width, setWidth] = useState(100);
+  const [height, setHeight] = useState(50);
+  const rectRef = useRef();
+  const handelIncWidth = () => {
+    setWidth(width + 15);
+  };
+  const handelDecWidth = () => {
+    setWidth(width - 15);
+  };
+  const handelIncHeight = () => {
+    setHeight(height + 15);
+  };
+  const handelDecHeight = () => {
+    setHeight(height - 15);
+  };
+
+  return (
+    <>
+      <div>
+        <label>width : </label>
+        <button onClick={handelIncWidth}>+</button>
+        <button onClick={handelDecWidth}>-</button>
+      </div>
+      <div>
+        <label>height : </label>
+        <button onClick={handelIncHeight}>+ </button>
+        <button onClick={handelDecHeight}>-</button>
+      </div>
+      <Stage width={700} height={700}>
+        <Layer>
+          <Text text={text} x={10} y={10} fontSize={24} fill={"black"} />
+          <Rect
+            x={20}
+            y={100}
+            width={width}
+            height={height}
+            fill={"#00D2FF"}
+            stroke={"black"}
+            strokeWidth={2}
+            draggable
+          />
+        </Layer>
+      </Stage>
+    </>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
